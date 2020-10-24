6a118bb746d919f3c27ac1ea4ebb62ba7aeadffd
Serge Pille <serge@pille.be>
2020-10-24 15:22:06 +0200

    Add project files.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>();
+            
+            return View(customers);
+        }
+
+        // GET: Customers/Details/1
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
+
+
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            //return null;
+            return new List<Customer>
+            {
+                new Customer { Id = 0, Name= "Serge Pille"},
+                new Customer { Id = 1, Name= "Emilie Pille"},
+                new Customer { Id = 2, Name= "Arthur Pille"},
+                new Customer { Id = 3, Name= "Annelies Nys"},
+                new Customer { Id = 4, Name= "Peter Verdonck"},
+                new Customer { Id = 5, Name= "Bart Pattyn"},
+
+            };
+
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // Get: Movies
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        // Get: Movies/Details/1
+        public ActionResult Details(int id)
+        {
+            var movie = GetMovies().SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("Id = " + id);
+        }
+
+
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+
+        public List<Movie> GetMovies()
+        {
+            return new List<Movie>()
+            {
+                new Movie {Id = 0, Name="Shrek"},
+                new Movie {Id = 1, Name="Toy Story"},
+                new Movie {Id = 2, Name="Inside Out"},
+                new Movie {Id = 3, Name="The Incredibles"},
+                new Movie {Id = 4,Name="Chicken Run"},
+            };
+
+        }
+
+    }
+}
