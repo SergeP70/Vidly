fa0bab27b41b5b5852350139cec2c60492108d68
Serge Pille <serge@pille.be>
2020-10-24 20:29:35 +0200

    Excercise2

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.Entity;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,10 +10,20 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();    
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = new List<Customer>();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
             
             return View(customers);
         }
@@ -20,7 +31,7 @@ namespace Vidly.Controllers
         // GET: Customers/Details/1
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (customer == null)
             {
                 return HttpNotFound();
